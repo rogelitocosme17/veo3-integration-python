c4b472a97ed96480011ca9dd252c27ce7d67392f
rogelito <rogelitocosme17@gmail.com>
2025-10-05 22:59:38 +0800

    update the logic with operation id

--- gen.py
+++ gen.py
@@ -1,10 +1,40 @@
-import google.generativeai as genai
+from google import genai
+import time
+from datetime import datetime
+from google.genai import types
+from google.genai.types import GenerateVideosOperation
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import FileResponse
+from fastapi.staticfiles import StaticFiles
+from dotenv import load_dotenv
+import os
 
-# Replace with your actual API key
-client = genai.Client(api_key="...")
 
-prompt = """A close up of two people staring at a cryptic drawing on a wall, torchlight flickering.
-A man murmurs, 'This must be it. That's the secret code.' The woman looks at him and whispering excitedly, 'What did you find?'"""
+# Load the .env file
+load_dotenv()
+google_api_key = os.getenv("GOOGLE_API_KEY")
 
-print(f"Client Type: {type(client)}")
-print(f"Client Attributes: {vars(client)}")
+# Initialize the GenAI client
+client = genai.Client(api_key=google_api_key)
+
+operation_id = "models/veo-3.0-generate-001/operations/2ndklfg582ib"
+
+operation = GenerateVideosOperation(name=operation_id)
+
+operation = client.operations.get(operation)
+
+print("Operation", operation)
+print("Operation Status", operation.done)
+
+timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
+filename = f"generate_video_by_veo3_{timestamp}.mp4"
+filepath = f"static/{filename}"
+
+# Download the generated video.
+generated_video = operation.response.generated_videos[0]
+client.files.download(file=generated_video.video)
+generated_video.video.save(filepath)
+
+print("Generated video saved to dialogue_example.mp4")
--- test.py
+++ test.py
@@ -1,11 +1,17 @@
 from google import genai
 import time
 from google.genai import types
+from dotenv import load_dotenv
+import os
 
-client = genai.Client(api_key="...")
+# Load the .env file
+load_dotenv()
+google_api_key = os.getenv("GOOGLE_API_KEY")
 
-prompt = """A close up of two people staring at a cryptic drawing on a wall, torchlight flickering.
-A man murmurs, 'This must be it. That's the secret code.' The woman looks at him and whispering excitedly, 'What did you find?'"""
+# Initialize the GenAI client
+client = genai.Client(api_key=google_api_key)
+
+prompt = """A cute creature with snow leopard-like fur is walking in winter forest, 3D cartoon style render."""
 
 operation = client.models.generate_videos(
     model="veo-3.0-generate-001",
@@ -18,8 +24,21 @@ while not operation.done:
     time.sleep(10)
     operation = client.operations.get(operation)
 
+print("Operation", vars(operation))
+print("Operation Name", operation.name)
+print("Operation Done", operation.done)
+print("Operation Metadata", operation.metadata)
+
+operation_id = operation.response.generated_videos[0].video.uri
+print(f"Operation ID: {operation_id}")
+
+print("Video generation Result:", vars(operation.result))
+print("Video generation Response:", vars(operation.response))
+
 # Download the generated video.
 generated_video = operation.response.generated_videos[0]
+print("Generated Video", vars(generated_video))
+
 client.files.download(file=generated_video.video)
 generated_video.video.save("dialogue_example.mp4")
 print("Generated video saved to dialogue_example.mp4")
